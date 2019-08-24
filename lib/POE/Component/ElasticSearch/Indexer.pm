63903d40bd2c6c4a9820daabf6d71cbc5779af03
Brad Lhotsky <brad@divisionbyzero.net>
2019-08-23 19:24:41 -0700

    Don't collide with human aliases
    Using 'http' as an alias means that a user won't be able to use that ID.
    Instead, md5 a string + the epoch to create a unique alias that won't
    collide with the human space.

--- lib/POE/Component/ElasticSearch/Indexer.pm
+++ lib/POE/Component/ElasticSearch/Indexer.pm
@@ -7,6 +7,7 @@ use warnings;
 # VERSION
 
 use Const::Fast;
+use Digest::MD5 qw(md5_hex);
 use Digest::SHA1 qw(sha1_hex);
 use Fcntl qw(:flock);
 use HTTP::Request;
@@ -261,6 +262,8 @@ sub spawn {
         $CONFIG{MaxFailedRatio} = 0.8;
     }
 
+    # We need a unique session alias for our http client
+    my $http_session_alias = md5_hex( "elasticsearch http client" . time() );
     # Management Session
     my $session = POE::Session->create(
         inline_states => {
@@ -281,12 +284,13 @@ sub spawn {
             #resp_health        => \&resp_health,
         },
         heap => {
-            cfg       => \%CONFIG,
-            stats     => {},
-            start     => {},
-            batch     => {},
-            health    => '',
-            es_ready  => 0,
+            cfg        => \%CONFIG,
+            stats      => {},
+            start      => {},
+            batch      => {},
+            health     => '',
+            es_ready   => 0,
+            http_alias => $http_session_alias,
         },
     );
 
@@ -302,7 +306,7 @@ sub spawn {
 
     my $http_timeout = $pool ? $CONFIG{Timeout} * $CONFIG{MaxConnsPerServer} : $CONFIG{Timeout};
     POE::Component::Client::HTTP->spawn(
-        Alias   => 'http',
+        Alias   => $http_session_alias,
         Timeout => $http_timeout,
         # Are we using Connection Pooling?
         $pool ? (ConnectionManager => $pool)  : (),
@@ -366,7 +370,7 @@ sub _stats {
     my $failure = $stats->{bulk_failure} || 0;
 
     # Fetch the pending request count from the HTTP client
-    $stats->{pending_requests} = $kernel->call( http => 'pending_requests_count' );
+    $stats->{pending_requests} = $kernel->call( $heap->{http_alias} => 'pending_requests_count' );
 
     # We tried stuff this go around
     if( $success >= 0 && $failure == 0 ) {
@@ -704,7 +708,7 @@ sub es_batch {
         $uri->as_string,
         $id,
     );
-    $kernel->post( http => request => resp_bulk => $req => $id );
+    $kernel->post( $heap->{http_alias} => request => resp_bulk => $req => $id );
     # Record the request
     $heap->{start}{$id} = time unless exists $heap->{start}{$id};
     $heap->{stats}{http_req} ||= 0;
